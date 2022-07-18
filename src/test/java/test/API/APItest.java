1a06209fd53bf42056ffb912fd5d30c3d5c57ac7
gpjacob-pki <george.pjacob@perkinelmer.com>
2022-07-18 13:32:06 +0530

    Added few more APIs, updated the BaseURL and validating whether it is JSON response

--- src/test/java/test/API/APItest.java
+++ src/test/java/test/API/APItest.java
@@ -1,10 +1,13 @@
 package test.API;
+import com.mongodb.util.JSON;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
-import net.bytebuddy.NamingStrategy.SuffixingRandom.BaseNameResolver.ForGivenType;
+
+import io.restassured.response.Response;
 import org.testng.Assert;
 import test.Utility.BaseClass;
 
@@ -14,7 +17,6 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.xml.crypto.Data;
-
 import org.hamcrest.Matchers.*;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -31,17 +33,51 @@ public class APItest extends BaseClass {
 	public void apimethod(String BaseURI,String Method,String Header_Key,String Header_Value,String status_code, String Name) {
 
 		logger=extent.startTest("Validating the "+Name+"API");
-		int statuscode=Integer.parseInt(status_code);  
-		RestAssured.baseURI=BaseURI;
+		int statuscode=Integer.parseInt(status_code);
+
+		String base = ConfigFileReader("URL")+BaseURI;
+
+		System.out.println("The base is :"+base);
+		RestAssured.baseURI=base;
 
 
-		String response=given().header(Header_Key,Header_Value).when().get().then().log().all().statusCode(statuscode).extract().response().asString();
-		
+		Response response=given().header(Header_Key,Header_Value).when().get().then().log().all().statusCode(statuscode).extract().response();
+
+
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, statuscode);
 		logger.log(LogStatus.PASS, "Response code is as expected:"+statuscode);
 
-		
+
+         String responseBody = response.getBody().asPrettyString();
+
+
+		 Assert.assertTrue(isJsonString(responseBody), "The response is JSON content");
+
+		/*int length=js.getInt("data.size()");
+
+
+
+		for(int i=0;i<length;i++) {
+			String experimentidActual=js.getString("data["+i+"].id");
+			if(experimentidActual.equals(Validationid)) {
+
+				nameofid=js.getString("data["+i+"].attributes.name");
+				logger.log(LogStatus.PASS, "experiment id: "+experimentidActual+" name is :"+nameofid);
+				break;
+			}
+			}*/
 	}
 
+
+	public boolean isJsonString(String response) {
+		try {
+			JSON.parse(response);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
 	 
 	 @DataProvider
 	 public String[][] postAPIData() throws IOException{
@@ -51,7 +87,7 @@ public class APItest extends BaseClass {
 		 if(ConfigFileReader("runon").equalsIgnoreCase("local")) {
 			  
 				  if(ConfigFileReader("environment").equalsIgnoreCase("srv18")) {
-			  testOBJArray=getdata(testdatasheetpath,"GetAPI");
+			  testOBJArray=getdata(testdatasheetpath,"GetAPIS");
 			  
 				  }
 				  //write for other env here
@@ -61,7 +97,7 @@ public class APItest extends BaseClass {
 			 
 				 if(System.getenv("environment").equalsIgnoreCase("srv18")) {
 					 System.out.println("executing with jenkin variables dataprovider in srv18");
-					 testOBJArray=getdata(testdatasheetpath,"GetAPI");
+					 testOBJArray=getdata(testdatasheetpath,"GetAPIS");
 				 }
 			 //write for other env here
 		 }
